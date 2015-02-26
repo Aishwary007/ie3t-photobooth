dad1359023789537061c622f93ae0aed40143d1c
Abhinav Kumar <abhinavkumar@outlook.com>
2015-02-27 03:52:35 +0530

    Remove all traces of PHP

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/rahul/apache
-touch file1.f
-#gphoto2 --capture-image-and-download --quiet
-feh -t -x -Sfilename -E 750 -y 1400 -W 2000 capt0000.jpg
-python pkill.py
-python start.py
-touch file2.f
